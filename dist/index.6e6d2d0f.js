bc4470334b6d064e8e55d3e4b00ca75874725696
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-03-20 21:50:42 +0100

    20.03.2023

--- dist/index.6e6d2d0f.js
+++ dist/index.6e6d2d0f.js
@@ -1,2 +1,28 @@
+"use strict";
+// section3 Wyszukiwanie elementów
+const listWithId = document.querySelector("#menu");
+listWithId.style.textTransform = "uppercase";
+listWithId.style.fontSize = "24px";
+console.log(listWithId);
+const listWithClass = document.querySelector(".menu");
+console.log(listWithClass);
+const menuItemsByTagName = document.querySelectorAll("li");
+console.log(menuItemsByTagName);
+const menuItemsByClass = document.querySelectorAll(".menu-item");
+console.log(menuItemsByClass);
+const firstMenuItem = document.querySelector(".menu-item");
+firstMenuItem.style.color = "tomato";
+console.log(firstMenuItem);
+// section4 Właściwości i atrybuty
+const image = document.querySelector(".image");
+console.log(image.src); // https://picsum.photos/id/9/320/240
+console.log(image.alt); // A laptop
+image.src = "https://picsum.photos/id/13/640/480";
+image.alt = "River bank";
+// section4 Właściwość textContent
+const textEl = document.querySelector(".article-text");
+console.log(textEl.textContent); // text inside p.article-text
+const titleEl = document.querySelector(".article-title");
+titleEl.textContent = "Welcome to Bahamas!";
 
 //# sourceMappingURL=index.6e6d2d0f.js.map
