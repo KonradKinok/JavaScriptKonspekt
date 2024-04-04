c03773598b842f9083ab4c86668b30f66aa6da52
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-04-04 23:50:47 +0200

    04.04.2024

--- dist/html/15-modulowoscKodu.b0eef612.js
+++ dist/html/15-modulowoscKodu.b0eef612.js
@@ -579,6 +579,11 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"aQ1Hw":[function(require,module,exports) {
+// const sqrt = Math.sqrt;
+// export const square = x => x * x;
+// export const diag = (x, y) => sqrt(square(x) + square(y));
+// export const diag2 = (x, y) => square(x) + square(y);
+// console.log(diag2(4, 3));
 
 },{}]},["ffDnG","aQ1Hw"], "aQ1Hw", "parcelRequiref4e4")
 
---
+++ src/scripts/15.mjs
@@ -0,0 +1,5 @@
+// const sqrt = Math.sqrt;
+// export const square = x => x * x;
+// export const diag = (x, y) => sqrt(square(x) + square(y));
+// export const diag2 = (x, y) => square(x) + square(y);
+// console.log(diag2(4, 3));
